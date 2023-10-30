8229010375b5661c2e1e6a67be75e788c90f9546
igor <323723@uwr.edu.pl>
2023-10-31 00:31:19 +0100

    feature#13: implement basic raymarching class and feature#15: implement primitive raymarching sphere

---
+++ Shadem/Headers/Objects/RMObject.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Object.h"
+
+class RMObject : public Object {
+
+protected:
+
+	//std::string sDF;
+	//std::string mainContent;
+
+	/*
+		// Do przysz³ej edycji obiektów
+		glm::vec3 Front;
+		glm::vec3 Up;
+		glm::vec3 Right;
+	*/
+
+
+	Shader shader;
+
+private:
+	
+
+	virtual void setupPrimitiveMesh() override;
+
+public:
+	RMObject(glm::vec3 position, glm::vec3 scale, const char* fShaderPath);
+	~RMObject() {
+	}
+
+	//bool LoadSDF(const std::string& path);
+	//bool LoadMainContent(const std::string& path);
+
+
+};
