6a2ef508fc9eec23a057b6ffa294db2f031032c9
G4zow4neMleko <graconin@gmail.com>
2023-11-05 17:15:28 +0100

    raymarching sphere works with a little bagged orientation after camera rotation, fix in progress

--- Shadem/Shaders/RMShaders/SphereFshader.glsl
+++ Shadem/Shaders/RMShaders/SphereFshader.glsl
@@ -10,33 +10,34 @@ uniform mat4 Projection;
 uniform vec3 CameraPos;
 uniform vec3 SpherePos;
 uniform float Zoom;
+uniform vec3 Forward;
 
 float distance_from_sphere(in vec3 p, in vec3 c, float r)
 {
 	return length(p - c) - r;
 }
 
-float map_the_world(in vec3 p)
+float map_the_world(in vec3 p, in double beta)
 {
-    float sphere = distance_from_sphere(p, SpherePos, 1.0);
+    float sphere = float(distance_from_sphere(p, SpherePos, 1.0) * beta);
 
     return sphere;
 }
 
-vec3 calculate_normal(in vec3 p)
+vec3 calculate_normal(in vec3 p, in double beta)
 {
     const vec3 small_step = vec3(0.001, 0.0, 0.0);
 
-    float gradient_x = map_the_world(p + small_step.xyy) - map_the_world(p - small_step.xyy);
-    float gradient_y = map_the_world(p + small_step.yxy) - map_the_world(p - small_step.yxy);
-    float gradient_z = map_the_world(p + small_step.yyx) - map_the_world(p - small_step.yyx);
+    float gradient_x = map_the_world(p + small_step.xyy, beta) - map_the_world(p - small_step.xyy, beta);
+    float gradient_y = map_the_world(p + small_step.yxy, beta) - map_the_world(p - small_step.yxy, beta);
+    float gradient_z = map_the_world(p + small_step.yyx, beta) - map_the_world(p - small_step.yyx, beta);
 
     vec3 normal = vec3(gradient_x, gradient_y, gradient_z);
 
     return normalize(normal);
 }
 
-vec4 ray_march(in vec3 ro, in vec3 rd)
+vec4 ray_march(in vec3 ro, in vec3 rd, in double beta)
 {
     float total_distance_traveled = 0.0;
     const int NUMBER_OF_STEPS = 32;
@@ -47,12 +48,12 @@ vec4 ray_march(in vec3 ro, in vec3 rd)
     {
         vec3 current_position = ro + total_distance_traveled * rd;
 
-        float distance_to_closest = map_the_world(current_position);
+        float distance_to_closest = map_the_world(current_position, beta);
 
         if (distance_to_closest < MINIMUM_HIT_DISTANCE)
         {
-            vec3 normal = calculate_normal(current_position);
-            vec3 light_position = vec3(2.0, -5.0, 3.0);
+            vec3 normal = calculate_normal(current_position, beta);
+            vec3 light_position = SpherePos + vec3(2.0, -5.0, 3.0);
             vec3 direction_to_light = normalize(current_position - light_position);
 
             float diffuse_intensity = max(0.0, dot(normal, direction_to_light));
@@ -73,11 +74,15 @@ void main()
 {
     vec2 res = vec2(1920.0f, 1080.0f);
 
-    vec2 uv = vec2(gl_FragCoord.x/res.x, gl_FragCoord.y/res.y) * 2 - 1;
-    //vec2 uv = vec2(gl_FragCoord.x, gl_FragCoord.y);
+    vec2 uv = (vec2(gl_FragCoord) - 0.5*res) / res.y;
+    //vec2 uv = vec2(FragPos);
     vec3 ro = CameraPos;
-    vec3 rd = vec3( normalize( vec4(uv, 45.0f/Zoom, 0.0) ));
+    vec3 rdBR = vec3(uv.x, uv.y, 45.0f/Zoom);
+    vec3 rd = normalize(vec3(View * vec4(rdBR, 0.0) ));
+
+
+    double beta = dot(rd , Forward);
     //rd = vec3(uv, 1.0);
 
-	FragColor = ray_march(ro, rd);
+	FragColor = ray_march(ro, rd, beta);
 }
--- Shadem/Source/Objects/Primitives.cpp
+++ Shadem/Source/Objects/Primitives.cpp
@@ -101,22 +101,38 @@ void Primitives::RMSphere::Draw(Camera &camera)
 {
 	shader.use();
 	//glm::mat4 model = glm::mat4(1.0f);
-	//model = glm::translate(model, position);
+	////model = glm::translate(model, position);
 	//model = glm::scale(model, scale);
 	//shader.setMat4("projection", projection);
 
 
-	glm::mat4 view = glm::mat4(1.0f);
-	view = glm::rotate(view, glm::radians(camera.Pitch), glm::vec3(1.0f, 0.0f, 0.0f));
-	view = glm::rotate(view, glm::radians(camera.Yaw), glm::vec3(0.0f, 1.0f, 0.0f));
+	//glm::mat4 view = glm::mat4(1.0f);
+	//view = glm::rotate(view, glm::radians(-camera.Pitch), glm::vec3(1.0f, 0.0f, 0.0f));
+	//view = glm::rotate(view, glm::radians(camera.Yaw - 90), glm::vec3(0.0f, 1.0f, 0.0f));
 
-	//glm::vec3 spherePos = rot * glm::vec4(camera.Position, 1.0f);
-	//shader.setMat4("View", glm::lookAt(glm::vec3(0.0f), camera.Position + camera.Front, camera.Front));
+	//glm::vec3 camPos = glm::vec3(camera.Position.x, camera.Position.y, camera.Position.z);
+	////shader.setMat4("View", glm::lookAt(glm::vec3(0.0f), camera.Position + camera.Front, camera.Front));
+
+	//float alpha = glm::dot(glm::vec3(0.0, 0.0, 1.0), camera.Front);
+
+	//view = glm::mat4(1.0f);
+	//view = glm::rotate(view, alpha, glm::cross(glm::vec3(0.0, 0.0, 1.0), camera.Front));
+
+	glm::mat4 view = glm::mat4{glm::vec4(camera.Right,0),
+					 glm::vec4(camera.Up,   0),
+					 glm::vec4(camera.Front,0), 
+					 glm::vec4(camera.Right,1)};
+	//view = glm::lookAt(camera.Position, camera.Position + camera.Front, camera.Up);
 	shader.setMat4("View", view);
 	shader.setVec3("CameraPos", camera.Position);
 	shader.setVec3("SpherePos", position);
 	shader.setFloat("Zoom", camera.Zoom);
 	shader.setMat4("Projection", glm::perspective(glm::radians(camera.Zoom), 1920.0f / 1080.0f, 0.1f, 250.0f));
+	shader.setVec3("Forward", camera.Front);
+
+	//std::cout << alpha << "\n";
+	//std::cout << "x: " << camera.Front.x << " y: " << camera.Front.y << " z: " << camera.Front.z << "\n";
+	//std::cout << "x: " << position.x << " y: " << position.y << " z: " << position.z << "\n";
 
 	mesh->Draw(shader);
 }
